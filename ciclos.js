ec7705c87f9a53f759651b73a3438a368da818ab
Alan Silva <60674021+AlanMSilva@users.noreply.github.com>
2020-02-19 23:02:06 -0600

    Add files via upload

---
+++ ciclos.js
@@ -0,0 +1,20 @@
+let s=0,y=1,fac=1,c=0;
+    for(let i=1;i<=3;i++){
+        y=Math.pow(y,i);
+        fac*=i;
+        s+=y/fac;
+    }
+console.log(s);
+
+
+console.log(exp(3));
+function exp(c){
+    var x=1,fac=1,y=1;
+    for(i=1;i<=c;i++)
+    {
+        y*=i;
+        x+=1/y
+        fac++;
+    }
+    return x;
+}
